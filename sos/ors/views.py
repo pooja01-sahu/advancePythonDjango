1e8897ff8fb69d3a911f42d8bf58bce805af48b8
pooja01-sahu <poojasahu11296@gmail.com>
2026-03-11 01:21:07 -0700

    django

---
+++ sos/ors/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def test_ors(request):
+    return HttpResponse("<h1>Hi my first project of ors not sos</h1>")
+# Create your views here.
