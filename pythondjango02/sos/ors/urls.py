5386a6e6482f85b94e927c7d966eaaa6149253d5
pooja01-sahu <poojasahu11296@gmail.com>
2026-03-29 01:42:00 -0700

    28-03-2026

--- pythondjango02/sos/ors/urls.py
+++ pythondjango02/sos/ors/urls.py
@@ -9,4 +9,13 @@ urlpatterns = [
     path('signup/', views.user_signup),
     path('signin/', views.user_signin),
     path('logout/', views.user_logout),
+    path('save/',views.add_marksheet),
+    path('list/', views.marksheet_list),
+    path('delete/<int:id>/',views.delete_marksheet),
+    path('edit/<int:id>/',views.edit_marksheet),
+    path('create/', views.create_session),
+    path('access/', views.access_session),
+    path('destroy/', views.destroy_session),
+    # path('set/', views.setCookies),
+    # path('get/', views.getCookies),
 ]
--- pythondjango02/sos/ors/views.py
+++ pythondjango02/sos/ors/views.py
@@ -1,3 +1,5 @@
+from django.contrib.sessions.models import Session
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -5,6 +7,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+
+from .models import Marksheet
 
 
 def welcome(request):
@@ -47,3 +51,67 @@ def user_signin(request):
 def user_logout(request):
     logout(request)
     return redirect('/ors/signin/')
+
+def add_marksheet(request):
+    message = ''
+    if request.method == "POST":
+        marksheet = Marksheet()
+        marksheet.rollNo = request.POST["rollNo"]
+        marksheet.name = request.POST["name"]
+        marksheet.physics = request.POST["physics"]
+        marksheet.chemistry = request.POST["chemistry"]
+        marksheet.maths = request.POST["maths"]
+
+        if request.POST['operation'] == "save":
+            message = 'Marksheet Added Successfully'
+        if request.POST['operation'] == "update":
+            marksheet.id = int(request.POST.get('id', 0))
+            message = 'Marksheet Updated Successfully'
+        if request.POST['operation'] == "list":
+            return redirect("/ors/list/")
+
+        marksheet.save()
+    return render(request, "marksheet.html", {'message': message})
+
+def marksheet_list(request):
+    list = Marksheet.objects.all()
+    return render(request, "marksheetlist.html", {"list": list})
+
+def delete_marksheet(request, id):
+    obj = Marksheet.objects.get(id=id)
+    obj.delete()
+    return redirect("/ors/list/")
+
+def edit_marksheet(request, id):
+    message = ''
+    obj = Marksheet.objects.get(id=id)
+    return render(request, "marksheet.html", {"form": obj, "id": id, 'message': message})
+
+def create_session(request):
+    request.session['name'] = 'Admin'
+    response = "<h1>Welcome To Sessions</h1><br>"
+    response += "ID : {0} <br>".format(request.session.session_key)
+    return HttpResponse(response)
+
+
+def access_session(request):
+    response = "Name : {0} <br>".format(request.session.get('name'))
+    return HttpResponse(response)
+
+
+def destroy_session(request):
+    Session.objects.all().delete()
+    return HttpResponse("Session is Destroy")
+
+def setCookies(request):
+    key = "name"
+    value = "abc"
+    res = HttpResponse("<h1>cookie created..!!</h1>")
+    res.set_cookie(key, value, max_age=20)
+    return res
+
+
+def getCookies(request):
+    value = request.COOKIES.get('name')
+    html = "<h3><center> value = {} </center></h3>".format(value)
+    return HttpResponse(html)
